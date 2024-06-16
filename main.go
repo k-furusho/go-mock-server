76ce5a54ea8e784badd7c57a761421466f06c06e
k-furusho <kfurusho0509@gmail.com>
2024-06-16 23:57:41 +0900

    build local mock

--- main.go
+++ main.go
@@ -1,4 +1,17 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
 func main()  {
 	http.HandleFunc("/", handleRequest)
 	http.ListenAndServe(":8080", nil)
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request)  {
+	byteArray,_ := ioutil.ReadFile("response.json")
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Write(byteArray)
 }
