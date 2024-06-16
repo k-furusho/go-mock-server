6c313a1d1565c377728fb2aac953c77c499b2b59
k-furusho <kfurusho0509@gmail.com>
2024-06-17 00:25:00 +0900

    add:error handling

--- main.go
+++ main.go
@@ -6,14 +6,17 @@ import (
 	"github.com/google/uuid"
 )
 
-func main()  {
+func main() {
 	http.HandleFunc("/", handleRequest)
 	http.ListenAndServe(":8080", nil)
 }
 
-func handleRequest(w http.ResponseWriter, r *http.Request)  {
+func handleRequest(w http.ResponseWriter, r *http.Request) {
 	uuidWithHyphen := uuid.New()
 	response := uuidWithHyphen.String()
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.Write([]byte(`{"uuid": "` + response + `"}`))
+	if _, err := w.Write([]byte(`{"uuid": "` + response + `"}`)); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
