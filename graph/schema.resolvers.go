20fb4d096472c9709fe3119ea8adf96537cc8816
k-furusho <kfurusho0509@gmail.com>
2024-06-17 22:42:51 +0900

    sample resolver

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -6,18 +6,43 @@ package graph
 
 import (
 	"context"
-	"fmt"
 	"go-mock-server/graph/model"
 )
 
-// CreateTodo is the resolver for the createTodo field.
+// ミューテーションcreateTodoが呼ばれた際に実行されるコード
 func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: CreateTodo - createTodo"))
+	return &model.Todo{
+		ID:   "TODO-3",
+		Text: input.Text,
+		Done: false,
+		User: &model.User{
+			ID:   input.UserID,
+			Name: "user1",
+		},
+	}, nil
 }
 
-// Todos is the resolver for the todos field.
+// クエリtodosが呼ばれた際に実行されるコード
 func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: Todos - todos"))
+	return []*model.Todo{
+		{
+			ID:   "TODO-1",
+			Text: "todo1",
+			User: &model.User{
+				ID:   "USER-1",
+				Name: "user1",
+			},
+			Done: true,
+		},{
+			ID:   "TODO-2",
+			Text: "todo2",
+			User: &model.User{
+				ID:   "USER-2",
+				Name: "user2",
+			},
+			Done: false,
+		},
+	},nil
 }
 
 // Mutation returns MutationResolver implementation.
--- server.go
+++ server.go
@@ -10,19 +10,22 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 )
 
-const defaultPort = "8080"
+const defaultPort = "8081"
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = defaultPort
+    }
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+    srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+    http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+    http.Handle("/query", srv)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+    log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+    err := http.ListenAndServe(":"+port, nil)
+    if err != nil {
+        log.Fatalf("サーバー起動エラー: %v", err)
+    }
 }
